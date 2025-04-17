e71f796b09cd58b573f8e1b0cc814302de9a987a
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-04-17 18:23:55 +0200

    feat: aggiunti post

--- src/pages/PostsList.jsx
+++ src/pages/PostsList.jsx
@@ -1,5 +1,30 @@
+// importazione file
+
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { Link } from "react-router-dom"
+
 export default function PostList(){
+    const [posts, setPosts]= useState([])
+
+    function postsList(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response=>{
+                setPosts(response.data)
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+    }
+
+    useEffect(postsList,[])
+
     return(
-        <div><h1>Lista dei Posts</h1></div>
+        posts.map(post=>(
+            <div className="post-container" key={post.id}>
+                <h2 className="post-title">{post.title}</h2>
+                <span className="link"><Link to={'/post/:id'}>Dettagli post</Link></span>
+            </div>
+        ))  
     )
 }
