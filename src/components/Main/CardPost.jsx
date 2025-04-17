888166da7bc517ca1544aceddce1b7dc6a5a394a
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-04-17 18:49:49 +0200

    feat: creato componenete card

---
+++ src/components/Main/CardPost.jsx
@@ -0,0 +1,13 @@
+// importazione file
+import { Link } from "react-router-dom"
+
+export default function CardPost({posts}){
+    return(
+        posts.map(post=>(
+            <div className="post-container" key={post.id}>
+                <h2 className="post-title">{post.title}</h2>
+                <span className="link"><Link to={`/posts/:${post.id}`}>Dettagli post</Link></span>
+            </div>
+        ))  
+    )
+}
